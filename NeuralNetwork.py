27427bf237dd95b659f541db614232e3c3955c2f
Alex Tornea <tornea.alexandru@gmail.com>
2020-01-28 14:42:55 +0200

    finish derivatives of activation functions

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -6,19 +6,21 @@ class NeuralNetwork:
     def __init__(self, layer_sizes):
         self.layer_sizes = layer_sizes
         self.layers = []
+        www = [[[0, 0], [0, 0]],
+               [[0, 0], [0, 0]]]
+        bbb = [[0, 0],
+               [0, 0]]
         for i in range(len(self.layer_sizes) - 1):
-            self.layers.append(NeuronLayer(self.layer_sizes[i + 1], self.layer_sizes[i]))
+            self.layers.append(NeuronLayer(self.layer_sizes[i + 1], self.layer_sizes[i], www[i], bbb[i]))
         self.cost = 0
 
     def predict(self, input_array):
         if len(input_array) != self.layer_sizes[0]:
-            err = f'Your input {input_array} does not match defined input layer size: {self.layer_sizes[0]}'
-            raise ValueError(err)
+            raise ValueError('Your input does not match defined input layer size.')
         tmp = input_array
         for k in range(len(self.layers)):
             print('layer', k)
             tmp = self.layers[k].forward(tmp)
-        # return self.layers[-1].out
         return self.layers[-1].get_output()
 
     def train(self, inputs, expected_output):
@@ -27,13 +29,14 @@ class NeuralNetwork:
         # https://stats.stackexchange.com/questions/154879/a-list-of-cost-functions-used-in-neural-networks-alongside-applications
         self.cost = 0
 
+        # todo: rename output with total net input
         for i in range(self.layer_sizes[-1]):
-            self.layers[-1].err[i] = self.layers[-1].out[i] - expected_output[i]
+            self.layers[-1].err[i] = self.layers[-1].total_net_input[i] - expected_output[i]
             # todo cost is calculated here
             self.cost += (self.layers[-1].err[i] ** 2) / 2
 
         for i in range(len(self.layers) - 1, 0, -1):
-            self.layers[i - 1].err = self.layers[i].train(self.layers[i - 1].out)
+            self.layers[i - 1].err = self.layers[i].train(self.layers[i - 1].total_net_input)
 
     def inspect(self):
         layer_number = 0
@@ -59,7 +62,7 @@ class NeuralNetwork:
             print(' ', end='\r')
             print(' ')
             print('output:'.ljust(8), end='')
-            for o in np.around(layer.out, decimals=2):
+            for o in np.around(layer.total_net_input, decimals=2):
                 print(str(o).rjust(8), end='')
             print('\n\nerror:'.ljust(10), end='')
             for e in np.around(layer.err, decimals=2):
@@ -68,18 +71,17 @@ class NeuralNetwork:
 
 
 class NeuronLayer:
-    def __init__(self, size, previous_layer_size, activation='sigmoid'):
+    def __init__(self, size, previous_layer_size, weights, bias, activation='sigmoid'):
         self.size = size
         self.pls = previous_layer_size
 
         # self.w = np.random.random((self.size, self.pls))
         # self.b = np.random.random(self.size)
         # todo remove below lines and uncomment above lines
-        self.w = np.array([[.5, .5, .5]])
-        self.b = np.array([.5])
+        self.w = weights
+        self.b = bias
 
-        # todo: rename output with total net input
-        self.out = [0] * self.size
+        self.total_net_input = [0] * self.size
         self.err = [0] * self.size
         self.learning_rate = .5
         self.activation = activation
@@ -89,47 +91,43 @@ class NeuronLayer:
             total = self.b[i]
             for j in range(self.pls):
                 total += inputs[j] * self.w[i][j]
-            self.out[i] = total
+            self.total_net_input[i] = total
         return self.get_output()
 
     def get_output(self):
-        out = []
-        for o in self.out:
-            out.append(self.squash(o))
-        return out
+        return [self.squash(o) for o in self.total_net_input]
 
-    def squash(self, x):
+    def squash(self, z):
+        """Squash the total net input"""
         if self.activation == 'sigmoid':
-            return 1 / (1 + math.exp(-x))
+            return 1 / (1 + math.exp(-z))
         elif self.activation == 'relu':
-            return max([0, x])
+            return max([0, z])
         elif self.activation == 'softsign':
-            return x / (1 + abs(x))
+            return z / (1 + abs(z))
         elif self.activation == 'softplus':
-            return math.log(1 + math.exp(x))
+            return math.log(1 + math.exp(z))
         else:
             raise ValueError('Not a valid activation function')
 
     def get_derivative(self, i):
-        # https://en.wikipedia.org/wiki/Activation_function
-        out = self.out
+        """Returns the derivative of the output in respect to total net input"""
+        tni = self.total_net_input
         if self.activation == 'sigmoid':
             sig = self.get_output()
             return sig[i] * (1 - sig[i])
         elif self.activation == 'relu':
-            return 0 if out[i] <= 0 else out[i]
+            return 0 if tni[i] <= 0 else tni[i]
         elif self.activation == 'softsign':
-            # todo de terminat aici
-            return 1
+            return 1 / (1 + abs(tni[i])) ** 2
         elif self.activation == 'softplus':
-            return 1 / (1 + math.exp(-out[i]))
+            return 1 / (1 + math.exp(-tni[i]))
 
     def train(self, inputs):
+        """Backpropagation"""
         previous_layer_error = [0] * self.pls
         for i in range(self.size):
-            # the derivative of the activation function of the current layer
-            derivative = self.get_derivative(i)
-            delta = self.err[i] * derivative
+            delta = self.err[i] * self.get_derivative(i)
 
             for j in range(self.pls):
                 previous_layer_error[j] += delta * self.w[i][j]
